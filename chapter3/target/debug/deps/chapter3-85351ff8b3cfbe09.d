262de15ed473c1417aaad14863e1e35b5e9babdb
michalshy <91937056+michalshy@users.noreply.github.com>
2024-06-28 17:39:15 +0200

    fix

--- chapter3/target/debug/deps/chapter3-85351ff8b3cfbe09.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Michin\Desktop\Rust\projects\chapter3\target\debug\deps\libchapter3-85351ff8b3cfbe09.rmeta: src/main.rs
-
-C:\Users\Michin\Desktop\Rust\projects\chapter3\target\debug\deps\chapter3-85351ff8b3cfbe09.d: src/main.rs
-
-src/main.rs:
--- chapter3/target/debug/deps/chapter3-cb1542744bf0809a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Michin\Desktop\Rust\projects\chapter3\target\debug\deps\libchapter3-cb1542744bf0809a.rmeta: src/main.rs
-
-C:\Users\Michin\Desktop\Rust\projects\chapter3\target\debug\deps\chapter3-cb1542744bf0809a.d: src/main.rs
-
-src/main.rs:
