19d7eb271fa60de2d9952f50ebc1e3cfdb2296d6
michalshy <91937056+michalshy@users.noreply.github.com>
2024-06-28 11:44:37 +0200

    init

---
+++ hello_cargo/target/debug/deps/hello_cargo-23e334639a572288.d
@@ -0,0 +1,5 @@
+C:\Users\Michin\Desktop\Rust\projects\hello_cargo\target\debug\deps\libhello_cargo-23e334639a572288.rmeta: src/main.rs
+
+C:\Users\Michin\Desktop\Rust\projects\hello_cargo\target\debug\deps\hello_cargo-23e334639a572288.d: src/main.rs
+
+src/main.rs:
