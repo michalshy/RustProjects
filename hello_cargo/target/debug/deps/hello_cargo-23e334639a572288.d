28152875ad1ac6f189b77a1e6ab42610f0efb29a
michalshy <91937056+michalshy@users.noreply.github.com>
2024-06-28 11:46:20 +0200

    fix

--- hello_cargo/target/debug/deps/hello_cargo-23e334639a572288.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Michin\Desktop\Rust\projects\hello_cargo\target\debug\deps\libhello_cargo-23e334639a572288.rmeta: src/main.rs
-
-C:\Users\Michin\Desktop\Rust\projects\hello_cargo\target\debug\deps\hello_cargo-23e334639a572288.d: src/main.rs
-
-src/main.rs:
